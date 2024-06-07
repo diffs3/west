a63e206bee32daf215ac9da7e947404980a7aadb
diffs3 <ahges8x@outlook.com>
2024-06-07 23:18:14 +0800

    Modify care.cpp

--- care.cpp
+++ care.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# All article too left.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
