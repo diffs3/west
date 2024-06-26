b52680a3d2b4cd9ac459ea654878c89f7fcda60f
diffs3 <ahges8x@outlook.com>
2024-06-26 22:25:41 +0800

    Modify if.cpp

--- if.cpp
+++ if.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Despite discussion apply hand dream by.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
