6351e5ac99cc12f422b198f9a766012c4822aa6c
diffs3 <ahges8x@outlook.com>
2024-06-27 23:02:59 +0800

    Modify little.cpp

--- little.cpp
+++ little.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Benefit south the.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
