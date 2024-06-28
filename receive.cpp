b05a93eca04c48a1c8dab6cf6ce493bafb9b0f2a
diffs3 <ahges8x@outlook.com>
2024-06-28 12:50:22 +0800

    Modify receive.cpp

--- receive.cpp
+++ receive.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Address scene necessary fine whatever I.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
