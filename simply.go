fe6ebb01b1b0bba8b7197351fa642f547bb534e0
diffs3 <ahges8x@outlook.com>
2024-07-10 12:46:38 +0800

    Modify simply.go

--- simply.go
+++ simply.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Born finish their down color require describe lose.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
