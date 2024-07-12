eb69823776ae4ba14a2cce3e0e1f1b7eaf453073
diffs3 <ahges8x@outlook.com>
2024-07-12 11:57:51 +0800

    Modify nor.cpp

--- nor.cpp
+++ nor.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Late mention gun actually again weight.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
