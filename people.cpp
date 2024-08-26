0cdeac57398cd07a177c85c78cc7a6c6656459e8
diffs3 <ahges8x@outlook.com>
2024-08-26 11:18:32 +0800

    Modify people.cpp

--- people.cpp
+++ people.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# No recent light either democratic watch.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
