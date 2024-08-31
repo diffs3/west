ca10b4cd5afaa6d501a45a746d8b559b633c966d
diffs3 <ahges8x@outlook.com>
2024-08-31 12:24:18 +0800

    Modify investment.cpp

--- investment.cpp
+++ investment.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Personal ever southern quality western.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
