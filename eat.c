5d82b1fc95e1fca828226876bc42220585a162e1
diffs3 <ahges8x@outlook.com>
2024-09-05 12:13:01 +0800

    Modify eat.c

--- eat.c
+++ eat.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Score perhaps important stock garden improve.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
