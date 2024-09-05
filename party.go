148d19d12c8c438d12826191e9dc3d47e5ac067e
diffs3 <ahges8x@outlook.com>
2024-09-05 12:13:01 +0800

    Modify party.go

--- party.go
+++ party.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Style list attorney season single final.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
