dab59dd3ff2ec8c7eb97ff0f9c72a6a0b057b87a
diffs3 <ahges8x@outlook.com>
2024-09-18 11:38:04 +0800

    Modify stand.cpp

--- stand.cpp
+++ stand.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# However student idea open number order doctor accept.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
