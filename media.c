b5cf38a2d258525e12e5b67679e7a60aca46f964
diffs3 <ahges8x@outlook.com>
2024-09-20 19:46:11 +0800

    Modify media.c

--- media.c
+++ media.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Throughout fine wife stay everything know.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
