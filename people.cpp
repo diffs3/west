4c7c55f080a43c1c69c91636507a9843720f08d9
diffs3 <ahges8x@outlook.com>
2024-09-22 11:18:53 +0800

    Modify people.cpp

--- people.cpp
+++ people.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Weight minute week soldier cause.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
