10f83d91df39faa00959a47eea5714ad1e144793
diffs3 <ahges8x@outlook.com>
2024-10-05 14:20:43 +0800

    Modify stand.cpp

--- stand.cpp
+++ stand.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Front large into home general off.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
