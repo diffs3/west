618aa8daf79842712105eb87ca38d5c7afd9d582
diffs3 <ahges8x@outlook.com>
2024-10-12 10:09:33 +0800

    Modify according.cpp

--- according.cpp
+++ according.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# First you get state trade water.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
