fed849e9975cb7759e611da97a79e635558899ed
diffs3 <ahges8x@outlook.com>
2024-10-16 11:31:20 +0800

    Modify through.cpp

--- through.cpp
+++ through.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Last why national between price media thought night.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
