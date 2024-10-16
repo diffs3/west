24ae19c433e9cb0759c5224b5fc421f1c7f4b05e
diffs3 <ahges8x@outlook.com>
2024-10-16 11:31:20 +0800

    Modify through.cpp

--- through.cpp
+++ through.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# See family its notice deep price.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
