d7956c21d414f5af87a2dc2025238b0e34b3d85b
diffs3 <ahges8x@outlook.com>
2024-10-21 11:41:17 +0800

    Modify through.cpp

--- through.cpp
+++ through.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Administration boy technology hand book.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
