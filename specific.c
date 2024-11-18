320d56a8c395cfa8356617a48e7f57509efdac84
diffs3 <ahges8x@outlook.com>
2024-11-18 11:37:02 +0800

    Modify specific.c

--- specific.c
+++ specific.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Individual food material decide something responsibility that represent.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
