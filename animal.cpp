71ff16d56fc46c4dfc04c1b114bc5c533ddc55ed
diffs3 <ahges8x@outlook.com>
2024-12-02 00:17:53 +0800

    Modify animal.cpp

--- animal.cpp
+++ animal.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Media film level.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
